3348f0b837d48fd96b67623f1ab54041c125ac73
erica27 <erica.eng@mail.utoronto.ca>
2021-11-06 16:48:52 -0400

    added get_all_deadlines

--- semesterCountdown.py
+++ semesterCountdown.py
@@ -2,6 +2,11 @@
 from datetime import datetime
 from typing import Union
 
+# user has to input date in correct format
+# we need deadline type: end of semester, test/exam or assignment or other?
+#     >>> get_countdown("calico", {"Calico176": ["23 November, 2021", "09 December 2021", "14 December 2021",
+#     "19 December 2021"], "JohnD" : ["13 October, 2021", "16 December 2021", })
+
 
 def get_countdown(deadline: str) -> Union[int, str]:
     """ Return the number of days left until the deadline
@@ -17,4 +22,14 @@ def get_countdown(deadline: str) -> Union[int, str]:
     if diff >= 0:
         return diff
     return "This date has already passed!"
+
+
+def get_all_deadlines(user: str, users: dict[str, list]) -> list[str]:
+    """ Return all deadlines for a specific user"""
+    deadlines = []
+    if len(users[user]) > 0:
+        for date in users[user]:
+            countdown = get_countdown(date)
+            deadlines.append(countdown)
+    return deadlines
 
